5ba74416f8ee5422688dedf9a87024cdecce2977
Prajakta <109715943+Prajaktasangolkar@users.noreply.github.com>
2023-05-27 23:20:58 +0530

    Create internship.py

---
+++ internship.py
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+@font-face {
+  font-family: 'HandwrittenFont';
+  src: url('handwritten_font.ttf') format('truetype');
+}
+
+body {
+  font-family: 'HandwrittenFont', cursive;
+}
+
+.equation {
+  font-family: 'HandwrittenFont', cursive;
+  font-size: 24px;
+  line-height: 1.2;
+}
+</style>
+</head>
+<body>
+  <p>x =  -b±√b<sup>2</sup>-4ac</p>
+  <div class="equation">
+    <div>
+      <span>√5</span>
+      <span>μ01</span>
+    </div>
+    <div>
+      <span>-------------------</span>
+      <span>2R</span>
+    </div>
+  </div>
+  <p>f(x)dx</p>
+</body>
+</html>
